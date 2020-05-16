8fe90fc6886e3d980c13b3d4523370f791a6c37d
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-05-16 14:17:15 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,6 +4,7 @@ printf "\e[31m
 環境エラー問題はvscodeのターミナルでは行わないでください
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\e[m
 "
+  EXIT=false
   return 2>&- || exit
 fi
 
@@ -77,3 +78,5 @@ source ~/projects/env_error_tools/db_backup.sh
 printf "
 【database】:バックアップ作成完了...
 "
+
+SETTINGS_COMPLETE=true
